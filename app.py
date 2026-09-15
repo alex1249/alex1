6baa7d69fdfdc840c180975f95e26618b89497e2
alex1249 <snirnam1234@gmail.com>
2026-09-15 21:13:34 +0900

    등록 라우트 + 폼

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import date
 
 app = Flask(__name__)
 
@@ -39,6 +40,42 @@ def material_detail(material_id):
     ).fetchall()
     conn.close()
     return render_template("material.html", material=material, originals=originals)
+
+@app.route("/subjects", methods=["POST"])
+def create_subject():
+    name = request.form["name"]
+    conn = get_db()
+    conn.execute("INSERT INTO subjects (name) VALUES (?)", (name,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
+
+
+@app.route("/subjects/<int:subject_id>/materials", methods=["POST"])
+def create_material(subject_id):
+    title = request.form["title"]
+    content = request.form["content"]
+    today = date.today().isoformat()
+    conn = get_db()
+    conn.execute(
+        "INSERT INTO materials (subject_id, title, content, date) VALUES (?,?,?,?)", (subject_id, title, content, today)
+    )
+    conn.commit()
+    conn.close()
+    return redirect(url_for("subject_detail", subject_id=subject_id))
+
+
+@app.route("/materials/<int:material_id>/originals", methods=["POST"])
+def create_otiginal(material_id):
+    label = request.form["label"]
+    content = request.form["content"]
+    conn = get_db()
+    conn.execute(
+        "INSERT INTO originals (material_id, label, contnet) BALUES (?, ?, ?),(material_id, label, content)"
+    )
+    conn.commit()
+    conn.close()
+    return redirect(url_for("material_detail", material_id=material_id))
 
 if __name__ == "__main__":
     app.run(debug=True)
